03e7636f4117e1fec20db10d7762bcc0e7c285f9
binarrii <karl50018@gmail.com>
2024-11-15 14:53:04 +0800

    Face Morph

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+import time
 import traceback
 
 from PIL import Image
@@ -7,20 +9,30 @@ from contrib.ComfyUI_fnodes.face_morph import FaceMorph
 from contrib.ComfyUI_fnodes.utils.image_convert import pil2tensor, tensor2pil, pil2np
 from deepface import DeepFace
 
-_CGREEN, _CRED, _CEND = "\033[92m", "\033[91m", "\033[97m"
 
-_SOURCE_IMAGE_PATH = "/mnt/onas/share4/frame_after_swap_for_post_processing/ep-测试/female"
-_TARGET_IMAGE_PATH = "/mnt/onas/share4/frame_after_swap_for_post_processing/ep-测试/female_predict_face"
-_RESULT_IMAGE_PATH = "/mnt/onas/share4/frame_after_swap_for_post_processing/ep-测试/female_morphed"
-
-_REF_FACE_PATH = "/mnt/onas/share4/frame_after_swap_for_post_processing/ep-测试/female.png"
+_CGREEN, _CRED, _YELLOW, _CEND = "\033[92m", "\033[91m", "\033[93m", "\033[97m"
 
 base_path = os.path.dirname(os.path.realpath(__file__))
 models_dir = os.path.join(base_path, "models")
 
 if __name__ == "__main__":
-    from ultralytics import YOLO
+    parser = argparse.ArgumentParser(description='Face Morph')
+    parser.add_argument('--workdir', required=True, type=str, default="")
+    parser.add_argument('--refface', required=True, type=str, default="")
+    args, _ = parser.parse_known_args()
 
+    _SOURCE_IMAGE_PATH = f"{args.workdir}/{args.refface}"
+    _TARGET_IMAGE_PATH = f"{args.workdir}/{args.refface}_predict_face"
+    _RESULT_IMAGE_PATH = f"{args.workdir}/{args.refface}_morphed_face"
+
+    if not os.path.exists(_RESULT_IMAGE_PATH):
+        os.makedirs(_RESULT_IMAGE_PATH, exist_ok=True)
+
+    _REF_FACE_PATH = f"{args.workdir}/{args.refface}.png"
+    if not os.path.exists(_REF_FACE_PATH):
+        _REF_FACE_PATH = f"{args.workdir}/{args.refface}.jpg"
+
+    from ultralytics import YOLO
     bbox_detector = YOLO(model=os.path.join(models_dir, "ultralytics/bbox/face_yolov8m.pt"))
 
     bbox_detect = BBoxDetect()
@@ -31,8 +43,9 @@ if __name__ == "__main__":
     ref_face_np = pil2np(Image.open(_REF_FACE_PATH).convert('RGB'))
 
 
-    def png_or_jpg(filename):
-        return filename.endswith('.png') or filename.endswith('.jpg')
+    def png_or_jpg(filename: str):
+        return (not filename.startswith('.')) and \
+            (filename.endswith('.png') or filename.endswith('.jpg'))
 
 
     for root, dirs, files in os.walk(_SOURCE_IMAGE_PATH):
@@ -47,7 +60,7 @@ if __name__ == "__main__":
                     image=source_image_tensor,
                     threshold=0.5,
                     dilation=20,
-                    dilation_ratio=0.35,
+                    dilation_ratio=0.5,
                     by_ratio=True,
                 )
                 if source_cnt <= 0:
@@ -59,32 +72,36 @@ if __name__ == "__main__":
                     image=target_image_tensor,
                     threshold=0.5,
                     dilation=20,
-                    dilation_ratio=0.35,
+                    dilation_ratio=0.5,
                     by_ratio=True,
                 )
                 if target_cnt <= 0:
                     print(f"{_CRED}[FaceMorph] no face detected in target: {_TARGET_IMAGE_PATH}/{f}{_CEND}")
                     continue
 
-                cropped_source_image_tensor, cropped_target_image_tensor = None, None
+                cropped_source_image_tensor, source_face_matched = None, False
                 for source_bbox in source_bboxes:
                     cropped_source_image_tensor, = image_crop_with_bbox.main(
                         bbox=source_bbox,
                         image=source_image_tensor
                     )
+                    t0 = time.time()
                     same_face = DeepFace.verify(
                         ref_face_np,
                         pil2np(tensor2pil(cropped_source_image_tensor)),
-                        model_name="Facenet512",
-                        detector_backend="yolov8",
+                        model_name="VGG-Face",
+                        detector_backend="skip",
                         enforce_detection=False
                     )
+                    print(f"{_YELLOW}[FaceMorph] face verify: {_SOURCE_IMAGE_PATH}/{f} {(time.time() - t0) * 1000}ms{_YELLOW}")
                     if same_face["verified"]:
+                        source_face_matched = True
                         break
-                if cropped_source_image_tensor is None:
+                if not source_face_matched:
                     print(f"{_CRED}[FaceMorph] no face matched in source: {_SOURCE_IMAGE_PATH}/{f}{_CEND}")
                     continue
 
+                cropped_target_image_tensor, target_face_matched = None, False
                 for target_bbox in target_bboxes:
                     cropped_target_image_tensor, = image_crop_with_bbox.main(
                         bbox=target_bbox,
@@ -93,16 +110,18 @@ if __name__ == "__main__":
                     same_face = DeepFace.verify(
                         ref_face_np,
                         pil2np(tensor2pil(cropped_target_image_tensor)),
-                        model_name="Facenet512",
-                        detector_backend="yolov8",
+                        model_name="VGG-Face",
+                        detector_backend="skip",
                         enforce_detection=False
                     )
                     if same_face["verified"]:
+                        target_face_matched = True
                         break
-                if cropped_target_image_tensor is None:
+                if not target_face_matched:
                     print(f"{_CRED}[FaceMorph] no face matched in target: {_TARGET_IMAGE_PATH}/{f}{_CEND}")
                     continue
 
+                t0 = time.time()
                 warped_image_tensor, = face_morph.execute(
                     source_image=cropped_source_image_tensor,
                     target_image=cropped_target_image_tensor,
@@ -110,8 +129,10 @@ if __name__ == "__main__":
                     align_type="Landmarks",
                     onnx_device="CUDA",
                 )
+                print(f"{_YELLOW}[FaceMorph] face warp: {_SOURCE_IMAGE_PATH}/{f} {(time.time() - t0) * 1000}ms{_YELLOW}")
+
                 final_image_tensor, = image_insert_with_bbox.main(
-                    bbox=target_bboxes[0],
+                    bbox=target_bbox,
                     image_src=source_image_tensor,
                     image=warped_image_tensor
                 )
@@ -122,3 +143,5 @@ if __name__ == "__main__":
                 print(f"{_CGREEN}[FaceMorph] result: {_RESULT_IMAGE_PATH}/{f}{_CEND}")
             except:
                 traceback.print_exc()
+                print(f"{_CRED}[FaceMorph] error occurred: {_SOURCE_IMAGE_PATH}/{f}{_CEND}")
+                print(f"{_CRED}[FaceMorph] error occurred: {_TARGET_IMAGE_PATH}/{f}{_CEND}")
